0ac880b80c31bca6b745eee97fa3406c056b3994
Wonky-Toad <ben.wright.whitlock@gmail.com>
2024-12-17 14:18:38 +0000

    New Mechanics
    Speed Boosts, Obstacles, stop/Start and Finish Line

--- JameGamChristmas2024/Assets/Scripts/PC/PC_Movement.cs
+++ JameGamChristmas2024/Assets/Scripts/PC/PC_Movement.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class PC_Movement : MonoBehaviour
 {
     float fl_speed = 5;
-    float fl_speed_multiplier = 2;
+    float fl_speed_multiplier = 1;
     float fl_gravity = 2;
+
+    float fl_speed_multiplier_history = 0; 
 
     public Vector3 v3_move_direction = Vector3.zero;
     public CharacterController cc_PC;
@@ -35,8 +37,8 @@ public class PC_Movement : MonoBehaviour
         // Convert world coordinates to local for the PC and multiply by speed
         v3_move_direction = fl_speed * fl_speed_multiplier * transform.TransformDirection(v3_move_direction);
 
-        // Add fl_gravity to the direction vector
-        v3_move_direction.y -= fl_gravity * Time.deltaTime * fl_speed_multiplier;
+        // Add fl_gravity to the direction vector - has been taken out for now
+        // v3_move_direction.y -= fl_gravity * Time.deltaTime * fl_speed_multiplier;
 
         // Move the character controller with the direction vector
         cc_PC.Move(v3_move_direction * Time.deltaTime);
@@ -46,8 +48,43 @@ public class PC_Movement : MonoBehaviour
     void OnTriggerEnter(Collider cl_trigger_collider)
     {
         if(cl_trigger_collider.gameObject.tag == "FinishLine") { Debug.Log("You Win!"); }
+
+        if(cl_trigger_collider.gameObject.tag == "SpeedBoost") 
+        {
+            Debug.Log("Past Speed Multiplier: " + fl_speed_multiplier);
+            fl_speed_multiplier++;
+            Debug.Log("Current Speed Multiplier: " + fl_speed_multiplier);
+
+        }
+
+        if (cl_trigger_collider.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Past Speed Multiplier: " + fl_speed_multiplier);
+            fl_speed_multiplier_history = fl_speed_multiplier;
+            StartCoroutine (Crash());
+            //fl_speed_multiplier = fl_speed_multiplier - 0.5f;
+
+            //SetSpeedMulti(1, 3);
+
+            //fl_speed_multiplier = fl_speed_multiplier - 0.5f;
+
+
+        }
     }
 
 
+    private IEnumerator Crash()
+    {
+        SetSpeedMulti(0); 
+        yield return new WaitForSeconds(3);
+        SetSpeedMulti(fl_speed_multiplier_history);
+
+    }
+
+    private void SetSpeedMulti(float newMulti) 
+    {
+        fl_speed_multiplier = newMulti;
+        Debug.Log("Current Speed Multiplier: " + fl_speed_multiplier);
+    }
 
 }
